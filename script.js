0e7ae5b41d0c5e5794a02722777be9185c4e5eec
Javier Moraga <jmoraga@buk.cl>
2024-12-21 17:57:54 -0300

    fix: message of setSquares()

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function removeSquares() {
 }
 
 function setSquares() {
-    squares = prompt("Number of squares:");
+    squares = prompt("Squares per row:");
     removeSquares();
     renderBoard(parseInt(squares));
 }
